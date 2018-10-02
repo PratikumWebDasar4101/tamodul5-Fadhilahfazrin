54c5057c098a16a9a39f57c4164edcb0caee9488
Fadhilah Fazrin <42725270+Fadhilahfazrin@users.noreply.github.com>
2018-10-02 22:00:03 +0700

    Update prosesLogin.php

--- prosesLogin.php
+++ prosesLogin.php
@@ -1,7 +1,12 @@
 <?php 
+session_start();
+$username = $_POST['username'];
+$password = $_POST['password'];
 
-
-
-
-
+if($username == "sayang" && $password =="1234"){
+	$_SESSION["username"] = $username;
+	header("Location: registrasi.php");
+}else{
+	header("Location: login.php");
+}
 ?>
