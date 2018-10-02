5aeda829e2f020dd360fb04f0be80bc94094fabd
Fadhilah Fazrin <42725270+Fadhilahfazrin@users.noreply.github.com>
2018-10-02 21:59:30 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,26 @@
-<?php
-
-
-
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>6701170001</title>
+</head>
+<body>
+	<h2>Selamat Datang!</h2>
+	<table>
+		<form action="prosesLogin.php" method="post">
+			<tr>
+				<td>Username</td>
+				<td>:</td>
+				<td><input type="text" name="username"></td>
+			</tr>
+			<tr>
+				<td>Password</td>
+				<td>:</td>
+				<td><input type="password" name="password"></td>
+			</tr>
+			<tr>
+				<td><input type="submit" name="submit" value="Masuk"></td>
+			</tr>
+		</form>
+	</table>
+</body>
+</html>
